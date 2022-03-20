1fc844648b70bc18e75b86eac847b362c89fec7d
Nicolas Enrique Debiaggi <nicodebiaggi@gmail.com>
2022-03-20 19:57:08 -0300

    .

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
 import express from 'express'
 import productRouter from './src/Routes/products.routes.js'
+import { products, getMaxId } from './src/Controllers/products.controllers.js'
+import path from 'path'
 
 const app = express()
 const PORT = 8080
+const __dirname = path.resolve()
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT} 🔥🔥🔥`)
 })
 app.use(express.json())
-app.use('/', productRouter)
+app.use(express.urlencoded({ extended: true }))
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/src/Views/Layouts')
+
+app.get('/products', (req, res) => {
+  res.render('layout', { products })
+})
+app.post('/products', (req, res) => {
+  const newProduct = {
+    title: req.body.title,
+    price: req.body.price,
+    thumbnail: req.body.thumbnail,
+    id: getMaxId() + 1
+  }
+  products.push(newProduct)
+  res.redirect('/products')
+})
+
+app.use('/api', productRouter)
 
 app.use(function (err, req, res, next) {
   console.log(err)
